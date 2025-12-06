61494be1f3bf9d4f192a98389fa094882c03be55
yingzibeimian <1239973004@qq.com>
2025-12-06 22:37:40 +0800

    Enhanced Input

---
+++ Source/AuraGAS_LearnProject/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright David He
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/AuraGAS_LearnProject/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright David He
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURAGAS_LEARNPROJECT_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
