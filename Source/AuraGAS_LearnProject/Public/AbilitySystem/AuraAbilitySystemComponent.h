471ec51aaaae05a6b125d5f36936d96fbf36f4ee
yingzibeimian <1239973004@qq.com>
2025-12-07 22:43:32 +0800

    Ability System Component and Attribute Set

---
+++ Source/AuraGAS_LearnProject/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Copyright David He
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AuraAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURAGAS_LEARNPROJECT_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/AuraGAS_LearnProject/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,17 @@
+// Copyright David He
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURAGAS_LEARNPROJECT_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
