d673a38fad2aaa8212c51c1243ef76a472bb04b4
yingzibeimian <1239973004@qq.com>
2025-12-08 23:30:08 +0800

    Attribute Accessors

--- Source/AuraGAS_LearnProject/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/AuraGAS_LearnProject/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -7,7 +7,10 @@
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-	
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(50.f);
+	InitMaxMana(50.f);
 }
 
 // Must be overridden if any variable to be replicate
