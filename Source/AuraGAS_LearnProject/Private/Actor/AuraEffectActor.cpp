1ee00755ca1d474f81ecf90eaacde146c523f922
yingzibeimian <1239973004@qq.com>
2025-12-09 14:58:00 +0800

    Effect Actor

---
+++ Source/AuraGAS_LearnProject/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,58 @@
+// Copyright David He
+
+
+#include "Actor/AuraEffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+AAuraEffectActor::AAuraEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(GetRootComponent());
+}
+
+/// 开始 Overlap 回调
+/// @param OverlappedComponent 自己的触发器
+/// @param OtherActor 触发Overlap 的对方物体
+/// @param OtherComp 对方触发 Overlap 的具体组件
+/// @param OtherBodyIndex 物理系统用的索引
+/// @param bFromSweep 该 Overlap 是否是因为物体“运动过程中”产生的
+/// @param SweepResult 命中结果
+void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                 UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		//TODO: Change this to apply a Gameplay Effect. For now, using const_cast as a hack!
+		const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+		// Should not set the variables directly on the AttributeSet(or its subclass), 
+		// AttributeSet should either set its own attribute values or change values in respond to GameplayEffect
+		// For learning purpose, we ast away the constantness of "AuraAttributeSet" and will fix this up to have a better design later
+		//AuraAttributeSet->SetHealth(AuraAttributeSet->GetHealth() + 25.f);
+		UAuraAttributeSet* MutableAuraAttributeSet = const_cast<UAuraAttributeSet*>(AuraAttributeSet);
+		MutableAuraAttributeSet->SetHealth(AuraAttributeSet->GetHealth() + 25.f);
+		Destroy();
+	}
+}
+
+void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	
+}
+
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
+}
+
