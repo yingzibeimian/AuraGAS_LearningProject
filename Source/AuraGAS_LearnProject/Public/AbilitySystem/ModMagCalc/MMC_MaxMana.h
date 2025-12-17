1415d3284a28d484a10cee009644a6df16985d98
yingzibeimian <1239973004@qq.com>
2025-12-17 17:14:34 +0800

    Modifier Magnitude Calculations

---
+++ Source/AuraGAS_LearnProject/Public/AbilitySystem/ModMagCalc/MMC_MaxMana.h
@@ -0,0 +1,24 @@
+// Copyright David He
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "MMC_MaxMana.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURAGAS_LEARNPROJECT_API UMMC_MaxMana : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+public:
+	UMMC_MaxMana();
+	
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+	
+private:
+	
+	FGameplayEffectAttributeCaptureDefinition IntelligenceDef;
+};
