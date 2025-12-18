9a241e54c8ac7b620cf9fc19b1835416fed4a24f
yingzibeimian <1239973004@qq.com>
2025-12-18 15:59:20 +0800

    Gameplay Tags Singletion

---
+++ Source/AuraGAS_LearnProject/Public/AuraGameplayTags.h
@@ -0,0 +1,24 @@
+// Copyright David He
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ * 
+ * Singleton containing native Gameplay Tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+	
+protected:
+	
+private:
+	static FAuraGameplayTags GameplayTags;
+};
