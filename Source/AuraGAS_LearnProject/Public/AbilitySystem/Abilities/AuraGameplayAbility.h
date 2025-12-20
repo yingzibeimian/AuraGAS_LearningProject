e3f34701cc23b40189a2eb74a65872c9b84507c4
yingzibeimian <1239973004@qq.com>
2025-12-20 15:31:55 +0800

    Activating Abilities

--- Source/AuraGAS_LearnProject/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
+++ Source/AuraGAS_LearnProject/Public/AbilitySystem/Abilities/AuraGameplayAbility.h
@@ -13,5 +13,8 @@ UCLASS()
 class AURAGAS_LEARNPROJECT_API UAuraGameplayAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
+public:
 	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	FGameplayTag StartupInputTag;
 };
