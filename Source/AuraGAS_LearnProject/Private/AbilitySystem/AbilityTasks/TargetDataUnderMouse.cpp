a5cc9b1dcb8893a9a0ff9ad7d84f189f2da3f921
yingzibeimian <1239973004@qq.com>
2025-12-21 19:51:42 +0800

    Send Mouse Cursor Data

--- Source/AuraGAS_LearnProject/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/AuraGAS_LearnProject/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
 
+#include "AbilitySystemComponent.h"
+
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
 	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
@@ -11,8 +13,44 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+	if (bIsLocallyControlled)
+	{
+		// On Server and being Locally Controlled: broadcasting the delegate
+		// On Client and being Locally Controlled: broadcasting the delegate, and also sending data to the Server
+		SendMouseCursorData();
+	}
+	else
+	{
+		//TODO: We are on the server, so listen for target data
+	}
+	
+
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	FScopedPredictionWindow ScopedPrediction(AbilitySystemComponent.Get());
+	
 	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
 	FHitResult CursorHit;
 	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
-	ValidData.Broadcast(CursorHit.Location);
+
+	FGameplayAbilityTargetDataHandle DataHandle;
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+	Data->HitResult = CursorHit;
+	DataHandle.Add(Data);
+	
+	AbilitySystemComponent->ServerSetReplicatedTargetData(
+		GetAbilitySpecHandle(), 
+		GetActivationPredictionKey(), 
+		DataHandle, 
+		FGameplayTag(), 
+		AbilitySystemComponent->ScopedPredictionKey);
+	
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
+
 }
