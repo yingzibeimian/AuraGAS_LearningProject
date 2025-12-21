5ce4ad54c91ea180cd090fda263c83319836b615
yingzibeimian <1239973004@qq.com>
2025-12-21 18:58:10 +0800

    Custom Ability Tasks

---
+++ Source/AuraGAS_LearnProject/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -0,0 +1,28 @@
+// Copyright David He
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "TargetDataUnderMouse.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FVector&, Data);
+/**
+ * 
+ */
+UCLASS()
+class AURAGAS_LEARNPROJECT_API UTargetDataUnderMouse : public UAbilityTask
+{
+	GENERATED_BODY()
+public:
+	
+	UFUNCTION(BlueprintCallable, Category="Ability|Tasks", meta=(DisplayName="TargetDataUnderMouse", HidePin="OwningAbility", DefaultToSelf="OwningAbility", BlueprintInternalUseOnly="true"))
+	static UTargetDataUnderMouse* CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility);
+	
+	UPROPERTY(BlueprintAssignable)
+	FMouseTargetDataSignature ValidData;
+	
+private:
+	
+	virtual void Activate() override;
+};
