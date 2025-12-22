821333886f1c8a029b03359555be6538f381b7d2
yingzibeimian <1239973004@qq.com>
2025-12-22 16:32:31 +0800

    Projectile Impact

--- Source/AuraGAS_LearnProject/Private/Actor/AuraProjectile.cpp
+++ Source/AuraGAS_LearnProject/Private/Actor/AuraProjectile.cpp
@@ -2,8 +2,11 @@
 
 
 #include "Actor/AuraProjectile.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 AAuraProjectile::AAuraProjectile()
 {
@@ -27,11 +30,44 @@ AAuraProjectile::AAuraProjectile()
 void AAuraProjectile::BeginPlay()
 {
 	Super::BeginPlay();
+	SetLifeSpan(LifeSpan);
 	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraProjectile::OnSphereOverlap);
+	
+	LoopingSoundComponent = UGameplayStatics::SpawnSoundAttached(LoopingSound, GetRootComponent());
+}
+
+void AAuraProjectile::Destroyed()
+{
+	// 防止碰撞后, 服务器已经Destroy, 且该权威行为现在Replicate到客户端时, 但客户端尚未发生碰撞： 此时直接播放碰撞音效和特效并Destroy
+	if (!bHit && !HasAuthority())
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation(), FRotator::ZeroRotator);
+		LoopingSoundComponent->Stop();
+	}
+	
+	Super::Destroyed();
 }
 
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+                                      UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// 防止客户端上OnSphereOverlap和Destroyed产生竞态时的两种情况
+	// 1. 先执行Destroyed, 则在Destroyed中播放碰撞音效和特效, 后续物体已经被销毁, OnSphereOverlap不会执行
+	// 2. 先执行OnSphereOverlap, 并标记为已碰撞, 后续Destroyed执行时就不会重复播放
+	if (bHit) return;
+	
+	UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation(), FRotator::ZeroRotator);
+	LoopingSoundComponent->Stop();
+	
+	if (HasAuthority())
+	{
+		Destroy();
+	}
+	else
+	{
+		bHit = true;
+	}
 }
 
