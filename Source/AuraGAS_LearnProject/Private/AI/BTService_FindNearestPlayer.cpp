b9bcda1a1a65721ab468c2273c6623521f326b2e
yingzibeimian <1239973004@qq.com>
2025-12-31 22:23:15 +0800

    Finding the Nearest Player

--- Source/AuraGAS_LearnProject/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/AuraGAS_LearnProject/Private/AI/BTService_FindNearestPlayer.cpp
@@ -4,6 +4,9 @@
 #include "AI/BTService_FindNearestPlayer.h"
 
 #include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Float.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Object.h"
 #include "Kismet/GameplayStatics.h"
 
 UBTService_FindNearestPlayer::UBTService_FindNearestPlayer()
@@ -21,7 +24,25 @@ void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, u
 	const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
 	
 	TArray<AActor*> ActorsWithTag;
+	
 	UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTag);
 	
+	float ClosestDistance = TNumericLimits<float>::Max();
+	AActor* ClosestActor = nullptr;
+	for (AActor* Actor : ActorsWithTag)
+	{
+		if (IsValid(Actor) && IsValid(OwningPawn))
+		{
+			const float Distance = OwningPawn->GetDistanceTo(Actor);
+			if (Distance < ClosestDistance)
+			{
+				ClosestDistance = Distance;
+				ClosestActor = Actor;
+			}
+		}
+	}
 	
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	BlackboardComp->SetValue<UBlackboardKeyType_Object>(TargetToFollowSelector.SelectedKeyName, ClosestActor);
+	BlackboardComp->SetValue<UBlackboardKeyType_Float>(DistanceToTargetSelector.SelectedKeyName, ClosestDistance);
 }
