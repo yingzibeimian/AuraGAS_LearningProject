763f3912fc78263b85cb0c12e2dc314eb889d452
yingzibeimian <1239973004@qq.com>
2026-01-05 17:31:14 +0800

    Melee Polish

--- Source/AuraGAS_LearnProject/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
+++ Source/AuraGAS_LearnProject/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -153,3 +153,10 @@ void UAuraAbilitySystemLibrary::GetLivePlayersWithinRadius(const UObject* WorldC
 		}
 	}
 }
+
+bool UAuraAbilitySystemLibrary::IsFriend(AActor* FirstActor, AActor* SecondActor)
+{
+	const bool bBothArePlayer = FirstActor->ActorHasTag(FName("Player")) && SecondActor->ActorHasTag(FName("Player"));
+	const bool bBothAreEnemy = FirstActor->ActorHasTag(FName("Enemy")) && SecondActor->ActorHasTag(FName("Enemy"));
+	return bBothArePlayer || bBothAreEnemy;
+}
--- Source/AuraGAS_LearnProject/Public/AbilitySystem/AuraAbilitySystemLibrary.h
+++ Source/AuraGAS_LearnProject/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -49,4 +49,7 @@ public:
 	
 	UFUNCTION(BlueprintCallable, Category="AuraAbilitySystemLibrary|GameplayMechanics")
 	static void GetLivePlayersWithinRadius(const UObject* WorldContextObject, TArray<AActor*>& OutOverlappingActors, const TArray<AActor*>& ActorsToIgnore, float Radius, const FVector& SphereOrigin);
+
+	UFUNCTION(BlueprintPure, Category="AuraAbilitySystemLibrary|GameplayMechanics")
+	static bool IsFriend(AActor* FirstActor, AActor* SecondActor);
 };
