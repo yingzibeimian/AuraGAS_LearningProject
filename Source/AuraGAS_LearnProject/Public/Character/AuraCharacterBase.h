d6c9d76fd1a3e77063cdd8fb568014c2df49bba1
yingzibeimian <1239973004@qq.com>
2026-01-08 12:57:53 +0800

    Demon Melee Attack

--- Source/AuraGAS_LearnProject/Public/Character/AuraCharacterBase.h
+++ Source/AuraGAS_LearnProject/Public/Character/AuraCharacterBase.h
@@ -63,6 +63,9 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Combat")
 	FName RightHandSocketName;
 	
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	FName TailSocketName;
+	
 	bool bDead = false;
 	
 	UPROPERTY()
