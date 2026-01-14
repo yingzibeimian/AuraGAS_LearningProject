43db1e56319ec821c66411f346f4950e7c91d5cd
yingzibeimian <1239973004@qq.com>
2026-01-14 17:54:13 +0800

    Top Off Our Fluids

--- Source/AuraGAS_LearnProject/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/AuraGAS_LearnProject/Public/AbilitySystem/AuraAttributeSet.h
@@ -66,6 +66,7 @@ public:
 	
 	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
 	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
 	
 	TMap<FGameplayTag, TStaticFuncPtr<FGameplayAttribute()>> TagsToAttributes;
 	
@@ -222,4 +223,6 @@ private:
 	void SetEffectProperties(const struct FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const;
 	void ShowFloatingText(const FEffectProperties& Props, float Damage, bool bBlockedHit, bool bCriticalHit) const;
 	void SendXPEvent(const FEffectProperties& Props);
+	bool bTopOffHealth = false;
+	bool bTopOffMana = false;
 };
