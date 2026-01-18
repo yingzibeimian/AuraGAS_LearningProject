1743bd8a383770cdc9ccb90f7646f6f423a4e1c5
yingzibeimian <1239973004@qq.com>
2026-01-19 01:23:32 +0800

    Cost and Cooldown in Spell Description

--- Source/AuraGAS_LearnProject/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
+++ Source/AuraGAS_LearnProject/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
@@ -5,6 +5,7 @@
 
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
+#include "AuraGameplayTags.h"
 
 void UAuraDamageGameplayAbility::CauseDamage(AActor* TargetActor)
 {
@@ -25,4 +26,10 @@ FTaggedMontage UAuraDamageGameplayAbility::GetRandomTaggedMontageFromArray(const
 		return TaggedMontages[Selection];
 	}
 	return FTaggedMontage();
+}
+
+float UAuraDamageGameplayAbility::GetDamageByDamageType(float InLevel, const FGameplayTag& DamageType)
+{
+	checkf(DamageTypes.Contains(DamageType), TEXT("GameplayAbility [%s] does not contain DamageType [%s]"), *GetNameSafe(this), *DamageType.ToString());
+	return DamageTypes[DamageType].GetValueAtLevel(InLevel);
 }
