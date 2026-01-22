189cb13cf65774acb22945bdcf3da26a43b82ea4
yingzibeimian <1239973004@qq.com>
2026-01-22 20:42:59 +0800

    Aura Beam Spell

---
+++ Source/AuraGAS_LearnProject/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -0,0 +1,25 @@
+// Copyright David He
+
+
+#include "AbilitySystem/Abilities/AuraBeamSpell.h"
+
+void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
+{
+	if (HitResult.bBlockingHit)
+	{
+		MouseHitLocation = HitResult.Location;
+		MouseHitActor = HitResult.GetActor();
+	}
+	else
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true);
+	}
+}
+
+void UAuraBeamSpell::StoreOwnerPlayerController()
+{
+	if (CurrentActorInfo)
+	{
+		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
+	}
+}
