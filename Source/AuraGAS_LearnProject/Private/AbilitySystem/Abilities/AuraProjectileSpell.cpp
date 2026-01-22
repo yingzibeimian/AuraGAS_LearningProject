bc555c70385f28f08068d1d4426fb3b0bfb5ed3e
yingzibeimian <1239973004@qq.com>
2026-01-22 15:07:26 +0800

    FireBolt Projectile Spread

--- Source/AuraGAS_LearnProject/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/AuraGAS_LearnProject/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -18,7 +18,7 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 
 void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride)
 {
-	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();;
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
 	
 	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(), SocketTag);
--- Source/AuraGAS_LearnProject/Public/AbilitySystem/Abilities/AuraFireBolt.h
+++ Source/AuraGAS_LearnProject/Public/AbilitySystem/Abilities/AuraFireBolt.h
@@ -15,5 +15,16 @@ class AURAGAS_LEARNPROJECT_API UAuraFireBolt : public UAuraProjectileSpell
 	GENERATED_BODY()
 public:
 	virtual FString GetDescription(int32 Level) override;
-	virtual FString GetNextLevelDescription(int32 Level) override;	
+	virtual FString GetNextLevelDescription(int32 Level) override;
+	
+	UFUNCTION(BlueprintCallable)
+	void SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride, AActor* HomingTarget);
+	
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category="FireBolt")
+	float ProjectileSpread = 90.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="FireBolt")
+	int32 MaxNumProjectiles = 5;
 };
