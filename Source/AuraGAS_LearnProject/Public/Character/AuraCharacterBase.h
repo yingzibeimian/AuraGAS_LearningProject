c99423469db040297d48b73b3e5ee27e57124118
yingzibeimian <1239973004@qq.com>
2026-01-23 00:40:25 +0800

    Gameplay Cue Notify Actor

--- Source/AuraGAS_LearnProject/Public/Character/AuraCharacterBase.h
+++ Source/AuraGAS_LearnProject/Public/Character/AuraCharacterBase.h
@@ -44,6 +44,7 @@ public:
 	virtual ECharacterClass GetCharacterClass_Implementation() override;
 	virtual FOnASCRegistered GetOnASCRegisteredDelegate() override;
 	virtual FOnDeath GetOnDeathDelegate() override;
+	virtual USkeletalMeshComponent* GetWeapon_Implementation() override;
 	/** Combat Interface */
 	
 	FOnASCRegistered OnAscRegistered;
