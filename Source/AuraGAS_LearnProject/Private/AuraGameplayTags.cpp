913ed837d7a66cc09a40fa65d76bbf15fb0b0fb2
yingzibeimian <1239973004@qq.com>
2026-01-24 23:33:14 +0800

    Passive Spell tags

--- Source/AuraGAS_LearnProject/Private/AuraGameplayTags.cpp
+++ Source/AuraGAS_LearnProject/Private/AuraGameplayTags.cpp
@@ -290,6 +290,25 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FString("Electrocute Ability Tag")
 		);
 	
+	/*
+	 * Passive Spells
+	 */
+	
+	GameplayTags.Abilities_Passive_LifeSiphon = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Abilities.Passive.LifeSiphon"), 
+		FString("Life Siphon")
+		);
+		
+	GameplayTags.Abilities_Passive_ManaSiphon = UGameplayTagsManager::Get().AddNativeGameplayTag(
+    	FName("Abilities.Passive.ManaSiphon"), 
+    	FString("Mana Siphon")
+    	);
+	
+	GameplayTags.Abilities_Passive_HaloOfProtection = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Abilities.Passive.HaloOfProtection"), 
+		FString("Halo of Protection")
+		);
+	
 	GameplayTags.Abilities_HitReact = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Abilities.HitReact"), 
 		FString("Hit React Ability")
