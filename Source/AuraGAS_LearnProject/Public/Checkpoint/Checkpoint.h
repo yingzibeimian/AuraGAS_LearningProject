91c98276c00ed65a5e88ec09f5f357551eb4372b
yingzibeimian <1239973004@qq.com>
2026-02-03 20:24:29 +0800

    Highlighting Non-Enemies

--- Source/AuraGAS_LearnProject/Public/Checkpoint/Checkpoint.h
+++ Source/AuraGAS_LearnProject/Public/Checkpoint/Checkpoint.h
@@ -38,10 +38,10 @@ protected:
 	
 	void HandleGlowEffect();
 	
-private:
-	
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
+	
+private:
 	
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USphereComponent> Sphere;
